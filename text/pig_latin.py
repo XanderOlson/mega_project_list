f1ee43eaae509b4eba0dfb91c16dd32b250b61f7
Xander <AlexanderM.Olson@gmail.com>
2015-06-15 11:11:33 -0700

    add code to translate words into pig latin add code to detect punctuation and keep it at the end of strings

--- text/pig_latin.py
+++ text/pig_latin.py
@@ -3,5 +3,22 @@ Pig Latin Translator
 Created: 6/15/2015
 """
 
+SUFFIX = 'ay'
+
 def main():
-	#TODO Project
+	# Prompt the user for English words
+	words_to_translate = raw_input("Please enter words you would like to translate to Pig Latin then hit enter:\n").split()
+	translated_words = []
+	for word in words_to_translate:
+		punctuation = ''
+		if not word[-1].isalpha(): #check for punctuation
+			# if there is punction, move to the end of the word
+			punctuation = word[-1]
+			word = word[:-1]
+			
+		translated_words.append(word[1:]+word[0]+SUFFIX+punctuation)
+
+	print ' '.join(translated_words)
+
+if __name__ == '__main__':
+	main()
