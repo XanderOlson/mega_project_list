973c5c26fac1f09688cc6c6f335b73905a0f6dad
Xander <AlexanderM.Olson@gmail.com>
2015-06-15 11:00:37 -0700

    add code to prompt user add code to reverse string add status and titles for user experience

--- text/reverse_string.py
+++ text/reverse_string.py
@@ -4,4 +4,12 @@ Created 6/15/2015
 """
 
 def main():
-	#TODO Reverse a String Project
+	#Prompt user for a string
+	input_string = raw_input("Please enter a string then hit 'Enter':\n")
+
+	print "\nReversing the string...\n" # Short status bar with spacing
+
+	print "Your reversed string:\n" + input_string[::-1] # Print reversed string
+
+if __name__ == '__main__':
+	main()
