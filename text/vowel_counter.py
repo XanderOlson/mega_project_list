6a5bfd8acb4514719ba5413d014ef646d78089ce
Xander <AlexanderM.Olson@gmail.com>
2015-06-15 17:47:00 -0700

    add code to count vowels add comments add loop for cleaner print

--- text/vowel_counter.py
+++ text/vowel_counter.py
@@ -2,7 +2,21 @@
 Vowel Counter
 Created: 06/15/2015
 """
+from collections import Counter
+
+VOWELS = 'AEIOU'
 
 def main():
-	# TODO Vowel Counter
+	# Prompt the user for a string
+	input_string = raw_input('Please enter a string and hit enter:\n')
+	
+	# Place all the characters into a dictionary
+	# Convert to lower case (so that 'A' is counted as 'a')
+	char_dict = Counter(input_string.lower())
+	print "\nThe numbers of vowels in your string:"
+	
+	for vowel in VOWELS:
+		print vowel + ':', char_dict[vowel.lower()]
 
+if __name__ == '__main__':
+	main()
