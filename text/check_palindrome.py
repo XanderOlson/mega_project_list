b95f17eb312c12f092aa5f819dace1e445165bec
Xander <AlexanderM.Olson@gmail.com>
2015-06-15 17:58:35 -0700

    add code to clean input strings add code to check if string is a palindrome and print result

--- text/check_palindrome.py
+++ text/check_palindrome.py
@@ -3,5 +3,21 @@ Palindrome Checker
 Created: 06/15/2015
 """
 
+import string
+
 def main():
-	# TODO Palidrome Checker
+	# Prompt user for input string
+	input_string = raw_input("Please enter a string and hit enter:\n")
+	# strip all punctuation from the string
+	cleaned_string = input_string.translate(string.maketrans("",""), string.punctuation)
+	# remove whitespaces
+	cleaned_string = cleaned_string.replace(' ', '')
+	
+	# check if the string is a palindrome
+	if cleaned_string == cleaned_string[::-1]:
+		print 'You entered a Palindrome!!!'
+	else:
+		print 'This is not a Palindrome...'
+
+if __name__ == '__main__':
+	main()
